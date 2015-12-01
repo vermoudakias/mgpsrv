e27d46ca8863d2152d6ebc8bad867d73bafc2353
Michael Manousos <mman@inaccess.com>
2015-12-01 11:45:13 +0200

    Unmarshal json response

--- timezonedb/timezonedb.go
+++ timezonedb/timezonedb.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type TimezoneDBResponse struct {
+	Status       string `json:"status"`
+	Message      string `json:"message"`
+	CountryCode  string `json:"countryCode"`
+	ZoneName     string `json:"zoneName"`
+	Abbreviation string `json:"abbreviation"`
+	GMTOffset    string `json:"gmtOffset"`
+	DST          string `json:"dst"`
+	Timestamp    int64  `json:"timestamp"`
+}
 
 func main() {
 	flag.Parse()
@@ -36,5 +48,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", data)
+	//fmt.Printf("%s\n", data)
+	timezoneDBres := TimezoneDBResponse{}
+	err = json.Unmarshal([]byte(data), &timezoneDBres)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("status=%s\n", timezoneDBres.Status)
+	fmt.Printf("message=%s\n", timezoneDBres.Message)
+	fmt.Printf("CC=%s\n", timezoneDBres.CountryCode)
+	fmt.Printf("zone=%s\n", timezoneDBres.ZoneName)
+	fmt.Printf("abbreviation=%s\n", timezoneDBres.Abbreviation)
+	fmt.Printf("gmtOffset=%s\n", timezoneDBres.GMTOffset)
+	fmt.Printf("dst=%s\n", timezoneDBres.DST)
+	fmt.Printf("timestamp=%d\n", timezoneDBres.Timestamp)
 }
