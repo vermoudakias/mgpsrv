92b37eaf193879153202c5ee5e7a20da649decdf
mike <vermoudakias@gmail.com>
2016-09-10 00:33:30 +0300

    Tutorial

--- tutorial/loops.go
+++ tutorial/loops.go
@@ -5,11 +5,18 @@ import (
 )
 
 func loopAsetup() []int {
-	array := make([]int, 10, 20)
+	// Make slice of int
+	var s1 = []int{2, 4, 6, 8}
+	fmt.Printf("Type %T, len %d, cap %d\n", s1, len(s1), cap(s1))
+	s1 = append(s1, 10)
+	fmt.Printf("Type %T, len %d, cap %d\n", s1, len(s1), cap(s1))
+	// Make slice of int
+	s := make([]int, 10, 20)
+	fmt.Printf("Type %T, len %d, cap %d\n", s, len(s), cap(s))
 	for i := 0; i < 10; i++ {
-		array[i] = 1970 + i * 10
+		s[i] = 1970 + i * 10
 	}
-	slice := array[:]
+	slice := s[:]
 	return slice
 }
 
@@ -17,5 +24,6 @@ func loopA(slice []int) {
 	for i,v := range slice {
 		fmt.Printf("[%d] %d\n", i, v)
 	}
+	fmt.Printf("slice %T: len %d, cap %d\n", slice, len(slice), cap(slice))
 }
 
